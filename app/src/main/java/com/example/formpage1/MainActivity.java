59f63916c39b5577f30c87e9853a22cc9ae9508e
yodeli32 <yxc15a@acu.edu>
2019-12-06 08:07:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/formpage1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.formpage1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendFeedback(View button) {
+        final EditText nameField = (EditText)findViewById(R.id.EditTextName);
+        String name = nameField.getText().toString();
+
+        final EditText emailField = (EditText)findViewById(R.id.EditTextEmail);
+        String email = emailField.getText().toString();
+
+        final EditText feedbackField = (EditText)findViewById(R.id.EditTextFeedbackBody);
+        String feedback = feedbackField.getText().toString();
+
+        final Spinner feedbackSpinner = (Spinner)findViewById(R.id.SpinnerFeedbackType);
+        String feedbackType = feedbackSpinner.getSelectedItem().toString();
+
+        final CheckBox responseCheckbox = (CheckBox)findViewById(R.id.CheckBoxResponse);
+        boolean bRequiresResponse = responseCheckbox.isChecked();
+    }
+}
